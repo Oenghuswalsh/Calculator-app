86493acb13717993982517288a8ac6f1426217fc
Oenghuswalsh <103225964+Oenghuswalsh@users.noreply.github.com>
2022-05-27 15:15:48 +1000

    more work on javascript

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const calculator = document.querySelector(".calculator")
 const keys = calculator.querySelector(".calculator__keys")
+const display = document.querySelector(".calculator__display")
 
 keys.addEventListener("click", e => {
     if (e.target.matches("button")) {
-       const key = e.target
-       const action = key.dataset.action
+const key = e.target
+const action = key.dataset.action
+const keyContent = key.textContent
+const displayedNum = display.textContent
+const previousKeyType = calculator.dataset.previousKeyType
+Array.from(key.parentNode.children)
+    .forEach(k => k.classList.remove("is-depressed"))
     if (!action) {
            console.log("number key!")
     } 
@@ -14,13 +20,48 @@ keys.addEventListener("click", e => {
         action === "multiply" ||
         action === "divide"
     ) {
+        key.classList.add("is-depressed")
+        calculator.dataset.previousKeyType = "operator"
         console.log("operator key")
+        calculator.dataset.firstValue = displayedNum
+        calculator.dataset.operator = action
+
+        display.textContent = calculate(firstValue, operator, secondValue)
+    }
+    if (action === "decimal") {
+        console.log("decimal key!")
+    }
+    if (action === "clear") {
+        console.log("clear key!")
+    }
+    if (action === "calculate") {
+        const firstValue = calculator.dataset.firstValue
+        const operator = calculator.dataset.operator
+        const secondValue = displayedNum
+        console.log("equal key!")
+    }
+    if (!action) {
+        if (displayedNum === "0") {
+            display.textContent = keyContent
+        } else {
+            display.textContent = displayedNum + keyContent
+        }
+    }
+    if (action === "decimal") {
+        display.textContent = displayedNum + "."
+    }
+    if (!action) {
+        if (displayedNum === "0" || previousKeyType === "operator") {
+            display.textContent = keyContent
+        } else {
+            display.textContent = displayedNum + keyContent
+        }
     }
 }
 })
 
 
-const calculate = (n1, operator, n2) => {
+/*const calculate = (n1, operator, n2) => {
     let result = ''
     if (operator === 'add') {
       result = parseFloat(n1) + parseFloat(n2)
@@ -86,5 +127,5 @@ const calculate = (n1, operator, n2) => {
         display.textContent = calculate(firstValue, operator, secondValue)
       }
     }
-  })
+  })*/
   
